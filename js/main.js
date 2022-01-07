5f14b63bd6ccb0a9acc8bcceb190221b59e5a770
naAlmanzor <neilalmanzor28@gmail.com>
2022-01-07 11:44:24 +0800

    Updated code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(800, 800);
+renderer.setPixelRatio(2.667);
 document.body.appendChild( renderer.domElement );
 
 // ---------------- Insert Code Here ---------------- // 
@@ -16,12 +17,15 @@ function dvd(){
 }
 dvd(scene);
 
+
 camera.position.z = 15;
 
 // ---------- Insert Animation Here ------------ //
 
 var dxPerFrameX = 0.1; // Moves cube on X axis
 var dxPerFrameY = 0.1; // Moves cube on Y axis
+
+var counter = 0; // tracks number of bounces
 
 let cube = scene.getObjectByName ("cube");
 
@@ -32,24 +36,52 @@ function animate() {
     cube.position.x += dxPerFrameX;
     cube.position.y += dxPerFrameY;
 
-    if (cube.position.x > 17){
+    
+    if (cube.position.x > 20 && counter < 5){
         dxPerFrameX = -0.10;
         cube.material.color.setRGB(Math.random(256), Math.random(256), Math.random(256));
+        cube.scale.x -= 0.2;
+        cube.scale.y -= 0.2;
+
+        counter++;
+        console.log(cube.scale.x);
     }
 
-    if (cube.position.x < -17){
+    if (cube.position.x < -20 && counter < 5){
         dxPerFrameX = 0.10;
         cube.material.color.setRGB(Math.random(256), Math.random(256), Math.random(256));
+        cube.scale.x -= 0.2;
+        cube.scale.y -= 0.2;
+
+        counter += 1;
+        console.log(cube.scale.x);
     }
 
-    if (cube.position.y > 10){
+    if (cube.position.y > 10 && counter < 5){
         dxPerFrameY = -0.10;
         cube.material.color.setRGB(Math.random(256), Math.random(256), Math.random(256));
+        cube.scale.x -= 0.2;
+        cube.scale.y -= 0.2;
+
+        counter++;
+        console.log(cube.scale.x);
     }
 
-    if (cube.position.y < -10){
+    if (cube.position.y < -10 && counter < 5){
         dxPerFrameY = 0.10;
         cube.material.color.setRGB(Math.random(256), Math.random(256), Math.random(256));
+        cube.scale.x -= 0.2;
+        cube.scale.y -= 0.2;
+
+        counter++;
+        console.log(cube.scale.x);
+    }
+
+    if (counter >= 5){
+        dxPerFrameX = -0.10;
+        cube.scale.set(0);
+        console.log(cube.scale.x);
     }
 }
 animate();
+
